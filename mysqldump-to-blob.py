a5b0c0562fa8054b32cd71eedae7388a8dd780b0
Peter Upfold <pgithub@upfold.org.uk>
2019-01-27 20:38:49 +0000

    initial commit

---
+++ mysqldump-to-blob.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# Mysqldump-to-blob
+#
+# Run a mysqldump and store as an Azure Blob Storage blob
+#
+# Copyright 2019 Peter Upfold
+#
+# Licensed under the Apache 2.0 Licence.
+
+import os, uuid, sys
+from azure.storage.blob import BlockBlobService
+import yaml
+import subprocess
+
+config_file = open('config.yml', 'r')
+config = yaml.safe_load(config_file)
+
+block_blob_service = BlockBlobService(account_name=config['account_name'], account_key=config['account_key'])
+
+# run mysqldump
+proc = subprocess.Popen(['/usr/bin/mysqldump', '-A', '--skip-extended-insert', '-r', config['mysqldump_file_output']], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, err = proc.communicate()
+
+
+if proc.returncode != 0:
+	print("Failed to run mysqldump")
+	# TODO some alerting?
+	sys.exit(proc.returncode)
+
+block_blob_service.create_blob_from_path(config['container_name'], config['blob_name'], config['mysqldump_file_output'])
+
